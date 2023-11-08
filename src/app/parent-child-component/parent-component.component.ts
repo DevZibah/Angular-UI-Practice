eeb04e1aaea978aac04da1d7a20cc7867f6f4f42
Chisom Chukwuma <chukwumachisom18@gmail.com>
2023-11-08 16:14:49 +0100

    nested the child in the parent component

--- src/app/parent-child-component/parent-component.component.ts
+++ src/app/parent-child-component/parent-component.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserDetails } from './interface';
+import { service } from './service';
 
 @Component({
   selector: 'app-parent-component',
@@ -7,8 +8,36 @@ import { UserDetails } from './interface';
   styleUrls: ['./parent-component.component.css'],
 })
 export class ParentComponentComponent implements OnInit {
+  errorMessage = '';
   users: UserDetails[] = [];
-  constructor() {}
+  constructor(private service: service) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Getting the users data immediately the app initializes
+    this.getData();
+  }
+
+  // this is a function(fn) to get all users from the fake database
+  private getData() {
+    this.service.getData().subscribe({
+      next: (data) => {
+        this.users = data;
+        console.log(this.users);
+      },
+      error: (err) => (this.errorMessage = err),
+    });
+  }
+
+  // viewDataById(viewTemplate: TemplateRef<any>, formUserId: number) {
+  //   if (formUserId) {
+  //     this.modalRef = this.modalService.show(viewTemplate);
+  //     this.formUser = this.formUsers.find((x) => x.id === formUserId);
+  //     console.log(this.formUser);
+
+  //     this.service.getDataById(formUserId).subscribe((response) => {
+  //       this.formUser = response;
+  //       console.log(this.formUser);
+  //     });
+  //   }
+  // }
 }
