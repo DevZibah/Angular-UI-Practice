dc79d2606f98a374f171f480ea66a9cf92f6c23e
Chisom Chukwuma <chukwumachisom18@gmail.com>
2024-02-08 05:00:13 +0100

    scroll-margin-top property

--- src/app/CSS-tricks/css-tricks/css-tricks.component.ts
+++ src/app/CSS-tricks/css-tricks/css-tricks.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-css-tricks',
@@ -6,16 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./css-tricks.component.css'],
 })
 export class CssTricksComponent {
-  showPopUp: boolean = false;
+  // showPopUp: boolean = false;
+  @ViewChild('homeSection', { read: ElementRef }) homeSection!: ElementRef;
+  @ViewChild('contactSection', { read: ElementRef })
+  contactSection!: ElementRef;
+  @ViewChild('aboutSection', { read: ElementRef }) aboutSection!: ElementRef;
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  openPopUpBox() {
-    this.showPopUp = true;
+  scrollTo(section: HTMLElement) {
+    section.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
-  closePopUpBox() {
-    this.showPopUp = false;
-  }
+
+  // openPopUpBox() {
+  //   this.showPopUp = true;
+  // }
+  // closePopUpBox() {
+  //   this.showPopUp = false;
+  // }
 }
